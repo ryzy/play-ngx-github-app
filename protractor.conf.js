bb6f0fe9cbeacf6eb4f93ac703a07147e08baf72
Marcin Ryzycki <marcin@ryzycki.com>
2017-10-17 19:58:35 +0100

    chore: karma.conf, tests fine-tuning

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,6 +1,7 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
+/*global jasmine */
 const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
@@ -21,7 +22,7 @@ exports.config = {
   },
   beforeLaunch: function() {
     require('ts-node').register({
-      project: 'e2e'
+      project: 'e2e',
     });
   },
   onPrepare() {
